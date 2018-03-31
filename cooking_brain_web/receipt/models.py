69c81aa5815034ea72f67c25c50b96cdc04b3daa
Mikhail Gordeev <gordeev.mv@gmail.com>
2018-03-31 23:26:52 +0300

    receipt django app

---
+++ cooking_brain_web/receipt/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+class QRRequest(models.Model):
+    user_id = models.CharField(max_length=1000)
+    date = models.DateField()
+    qr_code = models.CharField(max_length=1000)
+
+class ReceiptCash(models.Model):
+    fn = models.BigIntegerField()
+    fd = models.BigIntegerField()
+    fp = models.BigIntegerField()
+    receipt_raw = JSONField()
